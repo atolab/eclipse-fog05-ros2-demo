9dba76488eabbd42123fe01e4145ba6499310fee
gabrik <gabriele.baldoni@gmail.com>
2020-07-22 15:51:11 +0200

    fix typo and assing correct name of interfaces
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>

--- templates/gw/build.sh
+++ templates/gw/build.sh
@@ -3,18 +3,18 @@
 set -e
 
 sudo ip link add br0 type bridge
-sudo ip link up dev br0
+sudo ip link set up dev br0
 
 lxc profile copy default gw
 
-lxc profile device add gw eth1 nic nictype=bridged parent=br0 hwaddr=be:ef:be:ef:00:00
+lxc profile device add gw eth1 nic name=eth1 nictype=bridged parent=br0 hwaddr=be:ef:be:ef:00:00
 
 lxc launch images:alpine/edge gw -p gw
 sleep 5
 
 lxc exec gw -- apk update
 
-lxc exec gw -- apk add dnsmasq awalllx
+lxc exec gw -- apk add dnsmasq awall
 
 lxc file push ./interfaces gw/etc/network/interfaces
 lxc file push ./demo.conf gw/etc/dnsmasq.d/
@@ -27,9 +27,9 @@ lxc exec gw -- sh -c "yes | awall activate"
 lxc exec gw -- rc-update add iptables
 lxc exec gw -- rc-update add dnsmasq
 
-
-lxc stop gw
-lxc publish gw --alias gwimg
-lxc image export gwimg gw
-lxc image delete gwimg
-lxc delete gw
+lxc restart gw
+# lxc stop gw
+# lxc publish gw --alias gwimg
+# lxc image export gwimg gw
+# lxc image delete gwimg
+# lxc delete gw
--- templates/vap/build.sh
+++ templates/vap/build.sh
@@ -5,8 +5,8 @@ set -e
 lxc profile copy default ap
 
 lxc profile device remove ap eth0
-lxc profile device add ap eth0 nic nictype=bridged parent=br0 hwaddr=be:ef:be:ef:00:40
-lxc profile device add ap wlan1 nic nictype=physical parent=wlan1
+lxc profile device add ap eth0 nic name=eth0 nictype=bridged parent=br0 hwaddr=be:ef:be:ef:00:40
+lxc profile device add ap wlan1 nic name=wlan1 nictype=physical parent=wlan1
 
 lxc launch images:alpine/edge ap -p ap
 sleep 5
@@ -22,9 +22,9 @@ lxc file push ../templates/hostapd.conf ap/etc/hostapd/hostapd.conf
 
 lxc exec ap -- rc-update add hostapd
 
-
-lxc stop ap
-lxc publish ap --alias apimg
-lxc image export apimg ap
-lxc image delete apimg
-lxc delete ap
+lxc restart ap
+# lxc stop ap
+# lxc publish ap --alias apimg
+# lxc image export apimg ap
+# lxc image delete apimg
+# lxc delete ap
