82dafca5ea8ed71b20c918c4120f9f6d865b7d1c
gabrik <gabriele.baldoni@gmail.com>
2020-07-24 10:50:36 +0200

    using ubuntu as image for gui
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>

--- templates/gui/build.sh
+++ templates/gui/build.sh
@@ -3,19 +3,17 @@
 set -e
 
 
-lxc launch images:alpine/edge gui
+lxc launch images:ubuntu/bionic gui
 sleep 5
 
-lxc exec gui -- apk update
-lxc exec gui -- apk add nginx git
-lxc exec gui -- rc-update add nginx
-lxc exec gui -- sh -c "cd /root && git clone https://github.com/atolab/teleop_yaks"
-lxc exec gui -- sh -c "cp -r /root/teleop_yaks/gui/* /var/www/localhost/htdocs/"
-lxc exec gui -- sh -c "rm -rf /root/teleop_yaks"
+lxc exec gui -- apt update
+lxc exec gui -- apt install add nginx git -y
+lxc exec gui -- systemctl enablne nginx
+lxc exec gui -- bash -c "cd /root && git clone https://github.com/atolab/teleop_yaks"
+lxc exec gui -- bash -c "cp -r /root/teleop_yaks/gui/* /var/www/html/"
+lxc exec gui -- bash -c "rm -rf /root/teleop_yaks"
 
-lxc file push ./interfaces gui/etc/network/interfaces
-lxc file push ./resolv.conf gui/etc/resolv.conf
-lxc file push ./default.conf gui/etc/nginx/conf.d/default.conf
+lxc file push ./10-lxc.yaml gui/etc/netplan/10-lxc.yaml
 
 lxc stop gui
 lxc publish gui --alias guiimg
